8fb0f0fe6c931b0ced7fbfc4fca2a35650d99b70
Sarkis Moeradjan <smoeradjan@gmail.com>
2022-01-10 21:39:32 +0100

    added micro interaction

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
 // JavaScript Document
+
+const login = () => {
+    let loader = document.getElementById("loaderContainer");
+    loader.classList.remove("disabled");
+    setTimeout(
+        () => {
+            loader.classList.add("disabled");
+            window.location.href = 'index.html';
+        }, 2000);
+}
+
+const searchInput = () => {
+    let searchBackground = document.getElementsByClassName("searchingBackground")[0];
+    let searchResults = document.getElementsByClassName("searchResults")[0];
+    searchBackground.classList.remove("disabled");
+    searchResults.classList.remove("disabled");
+}
+
+const searchInputHandler = () => {
+    let searchBackground = document.getElementsByClassName("searchingBackground")[0];
+    let searchResults = document.getElementsByClassName("searchResults")[0];
+    let inputValue = document.getElementsByClassName('searchFieldInput')[0].value
+    let searchResultsList = document.getElementsByClassName('searchResultsList')[0]
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    let i = document.createElement('i')
+    i.className = "fas fa-times" 
+    span.appendChild(document.createTextNode(inputValue))
+    li.appendChild(span)
+    li.appendChild(i)
+    searchResultsList.appendChild(li);
+    searchBackground.classList.add("disabled");
+    searchResults.classList.add("disabled");
+}
